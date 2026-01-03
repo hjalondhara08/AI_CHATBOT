7784ff352df5d72d863ccaf262847226a53cfda8
HIREN JALONDHARA <jalondharahiren08@gmail.com>
2026-01-03 10:25:38 +0530

    this is AI CHATBOT

---
+++ backend_langgrraph.py
@@ -0,0 +1,74 @@
+from langgraph.graph import StateGraph,START,END
+from typing import TypedDict,Annotated
+from langchain_core.messages import HumanMessage,BaseMessage
+from langgraph.checkpoint.memory import InMemorySaver
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+
+ 
+
+# Load environment variables
+load_dotenv()
+
+# Read API key from .env
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(
+    api_key=os.getenv("GROQ_API_KEY"),
+    model="openai/gpt-oss-120b",
+)
+
+# print(llm.invoke("hi"))
+
+
+from langgraph.graph import message
+from langgraph.graph.message import add_messages
+from langgraph.graph.message import add_messages
+
+class ChatState(TypedDict):
+   messages : Annotated[list[BaseMessage],add_messages]
+
+checkpointer = InMemorySaver()
+thred_id = 1
+
+def chat_node(state: ChatState):
+
+    # take user query from state
+    messages = state['messages']
+
+    # send to llm
+    response = llm.invoke(messages)
+
+    # response store state
+    return {'messages': [response]}
+
+
+
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+initial_state = {
+    'messages': [HumanMessage(content='What is the capital of USA')],
+    'config': {"configurable": {"thread_id": "user-1"}}
+}
+
+# result = chatbot.invoke(initial_state, config={"configurable": {"thread_id": "user-1"}})
+# print("Result:", result)
+# print("Messages:", result['messages'])
+
+for message_chunk,metadata in chatbot.stream(
+  { 'messages':[HumanMessage(content="give me 70 line on ipl")]},
+  config={"configurable": {"thread_id": "1"}},
+  stream_mode="messages"
+):
+   if message_chunk.content :
+      print(message_chunk.content, end=" ", flush=True)
---
+++ frontend_streamlit.py
@@ -0,0 +1,38 @@
+from email import message
+import streamlit as st
+from backend_langgrraph import chatbot
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import InMemorySaver
+
+config = {"configurable": {"thread_id": "1"}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+
+user_input = st.chat_input("Enter your message:")
+
+if user_input:
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=config)
+    result = response['messages'][-1].content
+
+    st.session_state['message_history'].append({'role':'user','content':user_input})
+    with st.chat_message("user"):
+        st.write(user_input)
+    
+
+    
+    with st.chat_message("assistant"):
+
+     ai_message =  st.write_stream(
+      message_chunk.content for message_chunk,metadata in chatbot.stream(
+      { 'messages':[HumanMessage(content=user_input)]},
+      config={"configurable": {"thread_id": "1"}},
+      stream_mode="messages"
+    )
+    )
+    st.session_state['message_history'].append({'role':'AI','content':ai_message})
+
